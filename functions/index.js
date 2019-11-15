4c765792f6c11203ea0d4e775bde9ebc121078f1
Kamil Komnacki <kamilkomnacki@gmail.com>
2019-11-15 14:50:02 +0100

    init permissions server with examples fuctions

---
+++ functions/index.js
@@ -0,0 +1,181 @@
+const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors({origin: true}));
+var admin = require("firebase-admin");
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://permissionraport.firebaseio.com"
+});
+
+const db = admin.firestore();
+
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send(JSON.stringify("Hello from Firebase!"));
+});
+
+app.get('/hello-world', (req, res) => {
+    return res.status(200).send('Hello World!');
+});
+
+// create
+app.post('/api/create', (req, res) => {
+    (async () => {
+        try {
+            await db
+                .collection('items')
+                .doc(req.body.documentId)
+                .set({
+                    name: req.body.name,
+                    email: req.body.email
+                });
+            return res.status(200).send('OK');
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send('ERROR' + error);
+        }
+    })();
+});
+
+exports.app = functions.https.onRequest(app);
+
+/**
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+
+// Configure the email transport using the default SMTP transport and a GMail account.
+// For Gmail, enable these:
+// 1. https://www.google.com/settings/security/lesssecureapps
+// 2. https://accounts.google.com/DisplayUnlockCaptcha
+// For other types of transports such as Sendgrid see https://nodemailer.com/transports/
+// TODO: Configure the `gmail.email` and `gmail.password` Google Cloud environment variables.
+// const gmail.email="myusername@gmail.com" gmail.password="secretpassword"
+const gmailEmail = 'wapn.szkola@.gmail.com'/*functions.config().gmail.email;*/
+const gmailPassword = '65urszula'/*functions.config().gmail.password;*/
+const mailTransport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: gmailEmail,
+        pass: gmailPassword,
+    },
+});
+
+// Your company name to include in the emails
+// TODO: Change this to your app or company name to customize the email sent.
+const APP_NAME = 'Cloud Storage for Firebase quickstart';
+
+// [START sendWelcomeEmail]
+/**
+ * Sends a welcome email to new user.
+ */
+// [START onCreateTrigger]
+exports.sendWelcomeEmail2 = functions.https.onRequest((req, resp) => {
+    const email = 'wapn.szkola@gmail.com'; // The email of the user.
+    const displayName = "Kamil"; // The display name of the user.
+    // [END eventAttributes]
+    sendWelcomeEmail(email, displayName);
+})
+
+exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
+// [END onCreateTrigger]
+    // [START eventAttributes]
+    const email = user.email; // The email of the user.
+    const displayName = user.displayName; // The display name of the user.
+    // [END eventAttributes]
+
+    return sendWelcomeEmail(email, displayName);
+});
+
+//google account credentials used to send email
+var transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'wapn.szkola@gmail.com',
+        pass: 'c510mazowsze'
+    }
+});
+
+/*exports.sendEmail = functions.firestore
+    .document('orders/{orderId}')
+    .onCreate((snap, context) => {
+        const mailOptions = {
+            from: `Kamil`,
+            to: 'wapn.szkola@gmail.com',//snap.data().email,
+            subject: 'Test mail',
+            html: `<h1>Order Confirmation</h1>
+                                <p>
+                                   <b>Email: </b>${snap.data().email}<br>
+                                </p>`
+        };
+        return transporter.sendMail(mailOptions, (error, data) => {
+            if (error) {
+                console.log(error)
+                return
+            }
+            console.log("Sent!")
+        });
+    });*/
+
+exports.sendMailOverHTTP = functions.https.onRequest((req, res) => {
+    const mailOptions = {
+        from: `wapn.szkola@gmail.com`,
+        to: req.url.substring(req.url.indexOf("/") + 1),
+        subject: 'Test mail',
+        html: `<h1>Order Confirmation</h1>
+                            <p>
+                               <b>Email: </b>${req.body.mail}<br>
+                               <b>Email: </b>${req.body.email}<br>
+                               <b>Email: </b>${req.body.mailer}<br>
+                               <b>Email: </b>${req.query.mail}<br>
+                               <b>Email: </b>${req.query.email}<br>
+                               <b>Email: </b>${req.query.url}<br>
+                               <b>Email: </b>${req.body.url}<br>
+                               <b>Email: </b>${req.app}<br>
+                               <b>Email: </b>${req.url}<br>
+                               <b>Pozdro</b>
+                            </p>`
+    };
+    return transporter.sendMail(mailOptions, (error, data) => {
+        if (error) {
+            return res.send(error.toString());
+        }
+        var data = JSON.stringify(data)
+        return res.send(`Sent! ${data}`);
+    });
+});
+
+exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
+    .onCreate((snapshot, context) => {
+        // Grab the current value of what was written to the Realtime Database.
+        const original = snapshot.val();
+        console.log('Uppercasing', context.params.pushId, original);
+        const uppercase = original.toUpperCase();
+        // You must return a Promise when performing asynchronous tasks inside a Functions such as
+        // writing to the Firebase Realtime Database.
+        // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
+        return snapshot.ref.parent.child('uppercase').set(uppercase);
+    });
